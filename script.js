9cda26ce893243916c2ab4a5dad4411d419ee6be
Yasin <Yasinbas1@hotmail.com>
2023-04-29 17:37:04 +0300

    page index

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body"),
+  sidebar = body.querySelector(".sidebar"),
+  searchBtn = body.querySelector(".search-box"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".toggle-switch")
+
