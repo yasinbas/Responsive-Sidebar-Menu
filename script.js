287d513dbdb196cb8634188e043711fde3cd0002
Yasin <Yasinbas1@hotmail.com>
2023-04-29 18:53:04 +0300

    completed

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const body = document.querySelector("body"),
   modeSwitch = body.querySelector(".toggle-switch"),
   modeText = body.querySelector(".mode-text");
 
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
 
-  toggle.addEventListener("click" , () => {
-    sidebar.classList.toggle("close");
-  });
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-  modeSwitch.addEventListener("click" , () => {
-    body.classList.toggle("dark");
-  });
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light Mode";
+  } else {
+    modeText.innerText = "Dark Mode";
+  }
+});
